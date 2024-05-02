ee736199be02abad6bc0251c6607701202942d3c
saikumarpalistti <saikumarpalisetti797@gmail.com>
2024-05-02 19:22:34 +0530

    creating vector index with multiple documents

--- src/controllers/openai/DocBot.ts
+++ src/controllers/openai/DocBot.ts
@@ -1,4 +1,4 @@
-import { Document, MetadataMode, NodeWithScore, VectorStoreIndex } from "llamaindex";
+import { Document, MetadataMode, NodeWithScore, RouterQueryEngine, SimpleDirectoryReader, SummaryIndex, VectorStoreIndex, storageContextFromDefaults } from "llamaindex";
 import { Request, Response } from "express";
 
 import { promises as fs } from 'fs';
@@ -7,29 +7,43 @@ import path from 'path';
 export async function queryDocument(req: Request) {
 	try {
 		if(!process.env.OPENAI_API_KEY) throw new Error("No Open AI Key Available to call.")
-		const filePath = path.resolve(__dirname, 'test.md');
-		const essay = await fs.readFile(filePath, "utf-8");
-	
-		// Create Document object with essay
-		const document = new Document({ text: essay, id_: filePath });
-	
-		// Split text and create embeddings. Store them in a VectorStoreIndex
-		const index = await VectorStoreIndex.fromDocuments([document]);
-	
-		// Query the index
-		const queryEngine = index.asQueryEngine();
-		const { response, sourceNodes } = await queryEngine.query({
-			query: req.body.query,
-		});
-	
-		// Output response with sources
-		console.log(response);
-		if (sourceNodes) {
-			sourceNodes.forEach((source: NodeWithScore, index: number) => {
-				console.log(`\n${index}: Score: ${source.score} - ${source.node.getContent(MetadataMode.NONE).substring(0, 50)}...\n`);
-			});
-		}
-		return { response: response, sourceNodes: sourceNodes }
+		
+		const documents =  await new SimpleDirectoryReader().loadData({
+			directoryPath:path.resolve(__dirname, './storage') ,
+		  });
+             if(documents!==null)
+				{
+                   console.log("have conntent")
+				}
+		  const vectorIndex = await VectorStoreIndex.fromDocuments(documents);
+  const summaryIndex = await SummaryIndex.fromDocuments(documents);
+		
+  const vectorQueryEngine = vectorIndex.asQueryEngine();
+  const summaryQueryEngine = summaryIndex.asQueryEngine();
+
+  const queryEngine = RouterQueryEngine.fromDefaults({
+    queryEngineTools: [
+      {
+        queryEngine: vectorQueryEngine,
+        description: "Useful for summarization ",
+      },
+      {
+        queryEngine: summaryQueryEngine,
+        description: "Useful for retrieving specific data",
+      },
+    ],
+  });
+  
+ 
+  const summaryResponse = await queryEngine.query({
+    query:req.body.query
+  });
+
+  console.log({
+	answer: summaryResponse.response,
+	metadata: JSON.stringify(summaryResponse.metadata.selections)});
+// res.status(200).json({data:summaryRespons
+   
 	}
 	catch(err) {
 		console.error(err);
